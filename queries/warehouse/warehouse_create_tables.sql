8ed7cf61be28bcbc3f00258a6e95547c4fdc5747
Luděk Reif <ludek.reif@gmail.com>
2020-06-17 14:52:36 +0200

    added departments, positions and persons

--- queries/warehouse/warehouse_create_tables.sql
+++ queries/warehouse/warehouse_create_tables.sql
@@ -4,6 +4,18 @@ CREATE TABLE IF NOT EXISTS categories (
     name text NOT NULL
 );
 
+DROP TABLE IF EXISTS departments;
+CREATE TABLE IF NOT EXISTS departments (
+    id integer PRIMARY KEY,
+    name text NOT NULL
+);
+
+DROP TABLE IF EXISTS positions;
+CREATE TABLE IF NOT EXISTS positions (
+    id integer PRIMARY KEY,
+    name text NOT NULL
+);
+
 DROP TABLE IF EXISTS items;
 CREATE TABLE IF NOT EXISTS items (
     id integer PRIMARY KEY,
@@ -11,4 +23,18 @@ CREATE TABLE IF NOT EXISTS items (
     category_id integer NOT NULL,
     valid integer DEFAULT 1,
     FOREIGN KEY (category_id) REFERENCES categories (id)
+);
+
+DROP TABLE IF EXISTS persons;
+CREATE TABLE IF NOT EXISTS persons (
+    id integer PRIMARY KEY,
+    name text NOT NULL,
+    surname text NOT NULL,
+    title_before text NULL,
+    title_after text NULL,
+    department_id integer NOT NULL,
+    position_id integer NOT NULL,
+    active integer DEFAULT 1,
+    FOREIGN KEY (department_id) REFERENCES departments (id),
+    FOREIGN KEY (position_id) REFERENCES positions (id)
 );
